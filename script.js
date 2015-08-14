7fbf0c44451d7a86a12400186fc42efab92fd40f
jfisher1995 <jfisher1995@gmail.com>
2015-08-14 15:07:16 -0600

    added files and a lot of other stuff.

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 angular.module('movieApp', []).controller('movieCtrl',function($scope, $http){
-  var movieList = this;
-  movieList.movies = [
-    {title:'Pulp Fiction', url:"url"}
-  ];
-  movieList.addTitle = function() {
-    movieList.movies.push({text:movieList.movieTitle, url:"url"});
-    $http.get("http://www.omdbapi.com/?t=" + movieList.movieTitle + "&y=&plot=short&r=json")
-    .success(function(response) {console.log(response)});
-  }
+	var movieList = this;
+	console.log(localStorage);
+	movieList.movies = [];
+	if (window.localStorage.length != 0){
+		for (i in localStorage){
+			movieList.movies.push(JSON.parse(localStorage[i]));
+			console.log(localStorage[i].Title);
+		}
+	}
+	movieList.addMovie = function() {
+		var url = "http://www.omdbapi.com/?t=" + movieList.movieSearch + "&y=&plot=long&r=json";
+		$http.get(url).success(function(response){
+			console.log(response);
+			if(response.Response == "True"){
+				response.Poster = response.Poster.replace("SX300", "SX280");
+				movieList.movies.push(response);
+				localStorage[localStorage.length] = JSON.stringify(response);
+			}
+			else{
+				movieList.movies.push({Title: "No Movie Found", Plot:"N/A"});
+			}
+		});
+	}
+	movieList.clearMovies = function() {
+		window.localStorage.clear();
+		movieList.movies = [];
+	}
+	movieList.remove = function(i) {
+		movieList.movies.splice(i, 1);
+		localStorage.clear();
+		for (i in movieList.movies){
+			localStorage[localStorage.length] = JSON.stringify(movieList.movies[i]);
+		}
+	}
 });
 // $http.get("js.json").success(function (response){
 //         $scope.users = response.person;
